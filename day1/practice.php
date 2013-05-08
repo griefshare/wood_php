72ce59361e49435f1540b8d3cfa89c84b2366575
Dan Wood <dreasent@gmail.com>
2013-05-08 11:55:40 -0700

    adding more logic to my practice

--- day1/practice.php
+++ day1/practice.php
@@ -1,10 +1,40 @@
+<?php
+//create status var - possible values are 'success' or 'error'
+$status = 'success';
+
+ 
+//logic to control the message text based on status
+if( $status == 'success'){
+	$message = 'You rock!';
+}
+else{ 
+	$message = 'You must die';
+}
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
+<style>
+	.error {
+		background-color: #900;
+	}
+	.success {
+		background-color: #0F0;
+	}
+</style>
 </head>
 
 <body>
-	Hello World!
+	<div class="<?php echo $status; ?>">
+		<h2>
+	<?php 
+//this is a secret
+	echo $message; ?>
+	</h2>
+</div>
+
 </body>
 </html>
 
